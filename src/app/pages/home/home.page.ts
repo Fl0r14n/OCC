b7f03a039197baff2d4f5714611d3ecb0de9daf8
Florian Chis <florian.chis@gmail.com>
2017-12-06 12:07:38 +0200

    refactoring

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,22 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-home-page',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePageComponent implements OnInit {
-
-  // show the content of the homepage else the child pages
-  isHomepage = true;
-
-  constructor(protected router: Router) {
-  }
-
-  ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      // this.isHomepage = event.url === '/';
-    });
-  }
+export class HomePageComponent {
 }
